d0c0cfd99b690c76a48290937e512ddfc4ed7692
Emilio Piedra León <espiedra1@utpl.edu.ec>
2022-06-10 11:56:37 -0500

    Proyecto POO

---
+++ src/RunJugador.java
@@ -0,0 +1,10 @@
+public class RunJugador {
+    public static void main(String[] args) {
+        Jugador Jugador1=new Jugador(14,"Emilio","Base");
+
+        System.out.println(Jugador1.getNombre());
+        System.out.println(Jugador1.getNumero());
+        System.out.println(Jugador1.getPosicion());
+    }
+
+}
